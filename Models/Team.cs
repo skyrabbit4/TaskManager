be88554d4c73ba189ce5099162842f1b2943eb02
shubhamkaushik <shubham.kaushik004@gmail.com>
2026-03-15 17:02:38 +0530

    Add static web assets and update project dependencies for TaskManagerAPI
    - Created staticwebassets.build.json to manage static assets.
    - Added msbuild.build.TaskManagerAPI.props for static web assets integration.
    - Included msbuild.buildMultiTargeting.TaskManagerAPI.props for multi-targeting support.
    - Added msbuild.buildTransitive.TaskManagerAPI.props for transitive dependencies.
    - Generated project.assets.json to manage NuGet package dependencies.
    - Updated project.nuget.cache for package restoration tracking.
    - Configured TaskManagerAPI.csproj.nuget.g.props and TaskManagerAPI.csproj.nuget.g.targets for NuGet package imports.
    - Included necessary dependencies: Microsoft.AspNetCore.OpenApi and Swashbuckle.AspNetCore.

---
+++ Models/Team.cs
@@ -0,0 +1,14 @@
+namespace TASKMANAGERAPI.Models
+{
+    
+    public class Team
+    {
+        
+        public int Id {get; set;}
+        public  MyProperty { get; set; }
+        public string name { get; set; }
+        public 
+         MyProperty { get; set; }
+
+    }
+}
